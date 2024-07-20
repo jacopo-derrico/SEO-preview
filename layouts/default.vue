ccdfbe47213620bb31a214cd1422f22c69c80832
jacopo-derrico <derricoj.design@gmail.com>
2024-07-20 15:22:57 +0200

    fix google font import position

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,3 +30,7 @@
         </a>
     </footer>
 </template>
+
+<style lang="css">
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
