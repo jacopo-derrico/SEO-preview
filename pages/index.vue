c21e47f0759da9ea48c5793dce12ff26b945e659
jacopo-derrico <derricoj.design@gmail.com>
2024-07-20 15:09:16 +0200

    add tool description

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="mx-4 lg:w-9/12 lg:mx-auto">
         <p class="text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel vehicula sapien. Suspendisse ante dolor, imperdiet a odio mattis, faucibus venenatis nulla. Mauris porttitor massa sit amet nisi laoreet, et sollicitudin mi feugiat. Nunc a ante condimentum felis sagittis vulputate a fermentum lorem. Pellentesque mattis sem quisl.
+            This tool lets you preview the SEO of your website. Simply fill out the form with the title and description of your web page to see how it will look in a browser. You will see colored bars to help you understand if the length is optimal. It is more a question of pixel length than the number of characters. For any in-depth documentation, please refer to Google’s website documentation. Google’s bots may arbitrarily change the title and description depending on the search query.
+            <br>
+            Please note that while this tool provides an estimated preview, we do not guarantee its accuracy. We are not responsible for any discrepancies or issues that may arise from using this tool.
         </p>
     </div>
     <FormCard />
