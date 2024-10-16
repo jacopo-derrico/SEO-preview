abfb1883c7b45ededc2260b626e5e3c9028cd659
jacopo-derrico <derricoj.design@gmail.com>
2024-10-16 16:38:00 +0200

    build: disabled devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
+  app: {
+    head: {
+      title: 'SEO Preview',
+      meta: [
+        { name: 'description', content: 'Test the SEO for your website, see how it will show on browser and easily copy-paste it, or embed the code for free!'}
+      ]
+    }
+  }
 })
