e73a0062bf98e3257998e9c1025af95568a0a499
jacopo-derrico <derricoj.design@gmail.com>
2024-10-25 17:36:42 +0200

    doc: add link to google documentation

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <div class="my-14">
             <p class="text-center text-white font-semibold">
                 This tool lets you have a preview of the SEO of your website. Simply fill the form with the title and description of your website page to see what it will look like on browser
-                You will see coloured bars to help you understand if the length is good or bad. It is more a question of pixel length than number of characters, for any in-depth documentation please check Google’s website documentation.
+                You will see coloured bars to help you understand if the length is good or bad. It is more a question of pixel length than number of characters, for any in-depth documentation please check <a class="underline" href="https://developers.google.com/search/docs/fundamentals/seo-starter-guide">Google’s website documentation</a>.
                 Google’s bots may change the title and description arbitrary depending on the search that has been made.
             </p>
         </div>
@@ -22,5 +22,9 @@
         backdrop-filter: blur(8.9px);
         -webkit-backdrop-filter: blur(8.9px);
         border: 1px solid rgba(255, 255, 255, 0.6);
+    }
+
+    p {
+        filter: drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.2));
     }
 </style>
